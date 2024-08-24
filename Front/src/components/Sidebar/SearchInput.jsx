6802271ac362c786b499e55adefb651451245964
Udit Narayan Saxena <udit9407@gmail.com>
2024-08-25 01:57:00 +0530

    design part completed

---
+++ Front/src/components/Sidebar/SearchInput.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { IoIosSearch } from "react-icons/io";
+
+const SearchInput = () => {
+  return (
+    <div>
+      <form className="flex items-center gap-2">
+        <input
+          type="text"
+          placeholder="Search…"
+          className="input input-bordered rounded-full"
+        />
+        <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
+				 <IoIosSearch className="w-6 h-6 outline-none"/> 
+			</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchInput;
